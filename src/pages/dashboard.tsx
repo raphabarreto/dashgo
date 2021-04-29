2b6ddcfc4cde0f46b422447d0b80e10e7fc7af76
Raphael Barreto <tnt.raphael@gmail.com>
2021-04-29 16:30:54 -0300

    Corrigido estilização

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react';
+import { Flex, SimpleGrid, Box, Text, theme } from '@chakra-ui/react';
 import dynamic from 'next/dynamic';
 
 import { Header } from '../components/Header';
@@ -62,19 +62,21 @@ export default function Dashboard() {
   return (
     <Flex direction="column" h="100vh">
       <Header />
-      <Flex w="100%" maxW={1480} my="6" mx="auto" px="6">
+
+      <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
-          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
+
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
-              Taxa de abertura
+              Inscritos da semana
             </Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
