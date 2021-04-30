3885f4a3afaaefc9896bc53cc4d6c9f538a51ab3
Raphael Barreto <tnt.raphael@gmail.com>
2021-04-30 09:25:12 -0300

    Criação do Componente: Pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,6 +18,7 @@ import { RiAddLine, RiPencilFill } from 'react-icons/ri';
 
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
+import { Pagination } from '../../components/Pagination';
 
 export default function UserList() {
   return (
@@ -133,6 +134,8 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
